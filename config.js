a6370a8234bc28b3cfb4ace0a869252b259c31e2
David Peicho <david.peicho@gmail.com>
2019-09-11 00:13:32 +0100

    config.js: adding comments to point out what to change

--- config.js
+++ config.js
@@ -31,7 +31,9 @@ export const User = {
 };
 
 /**
- * Contains data for the entire blog.
+ * Contains data for the entire blog. Those options are mostly for SEO.
+ *
+ * NOTE: It's really important that you setup the `baseURL`.
  *
  * @property {string} name - The blog's name.
  * @property {string} description - The blog's description. This will basically
@@ -40,11 +42,24 @@ export const User = {
  * @property {string} googleAnalytics - Your Google Analytics website ID.
  */
 export const Site = {
+
+  /** Title of your blog. <meta title>. */
   name: 'David Peicho',
+
+  /** Description of your blog. <meta description>. */
   description: 'Blog about computer graphics, rendering, game developmnent, or just computer science!',
+
+  /** Blog keywords. <meta keywords>. */
   keywords: 'computergraphics, rendering, 3d',
+
+  /**
+   * The `baseURL` can be pretty important for SEO.
+   */
   baseURL: 'davidpeicho.github.io',
+
+  /** Your Google Analytics token. */
   googleAnalytics: 'UA-142103532-1'
+
 };
 
 /**
